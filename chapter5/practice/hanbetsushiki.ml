fb59f952f9547eb0037f09cc1b09a5cd163fefea
Hyuga Tsukui <gakikiki407@gmail.com>
2024-03-16 19:32:30 +0900

    practice 5.6

--- chapter5/practice/hanbetsushiki.ml
+++ chapter5/practice/hanbetsushiki.ml
@@ -21,3 +21,16 @@ let test1 = kai_no_kosuu 3.0 6.0 2.0 = 2
 let test2 = kai_no_kosuu 1.0 (-.6.0) 9.0 = 1
 let test3 = kai_no_kosuu (-.1.0) 3.0 (-.2.0) = 2
 let test4 = kai_no_kosuu 15.0 24.0 10.0 = 0 
+
+(* 目的:2次方程式 ax^2+bx+c=0 の係数a,b,cを受取り虚数解を持つかチェックする *)
+
+(* kyosuukai: float -> float -> float -> bool *)
+let kyosuukai a b c =
+        let d = hanbetsushiki a b c in
+        if d < 0.0 then true
+        else false
+
+(* テスト *)
+let test1 = kyosuukai 2.0 (-.3.0) 5.0 = true
+let test2 = kyosuukai 16.0 (-.24.0) 9.0 = false
+
