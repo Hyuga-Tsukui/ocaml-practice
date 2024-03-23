df590c5472ae88b5e0a33d49abf442331e89240e
Hyuga Tsukui <gakikiki407@gmail.com>
2024-03-23 21:23:54 +0900

    chapter10.4

--- chapter10/shukei.ml
+++ chapter10/shukei.ml
@@ -28,16 +28,14 @@ let rec shukei lst =
   match lst with
   | [] -> 0, 0, 0, 0
   | { namae = n; tensuu = t; seiseki = s } :: rest ->
-    let rest_shukei = shukei rest in
-    (match rest_shukei with
-     | a, b, c, d ->
+    let a, b, c, d = shukei rest in
        if s = "A"
        then a + 1, b, c, d
        else if s = "B"
        then a, b + 1, c, d
        else if s = "C"
        then a, b, c + 1, d
-       else a, b, c, d + 1)
+       else a, b, c, d + 1
 ;;
 
 (* tests *)
