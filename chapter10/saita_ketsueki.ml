2349b529f4480c3b680cceb357f5593779ec57a1
Hyuga Tsukui <gakikiki407@gmail.com>
2024-03-23 23:40:39 +0900

    practice 10.8

--- chapter10/saita_ketsueki.ml
+++ chapter10/saita_ketsueki.ml
@@ -1,24 +1,17 @@
 #use "ketsueki_shukei.ml"
 
-let rec max lst =
-  match lst with
-  | [] -> 0
-  | first :: rest ->
-    let m = max rest in
-    if first > m then first else m
-;;
-
-let max_test1 = max [ 1; 2; 4; 1 ] = 4
+let max a b = if a < b then b else a
 
 (* 目的: person_t 型のリストを受取り、4つの血液型のうち最も人数の多い血液型を返す *)
 (* saita_ketsueki : person_t list -> string *)
 let saita_ketsueki lst =
   let a, b, ab, o = ketsueki_syukei lst in
-  if max [ b; ab; o ] < a
+  let max_bl = max (max a b) (max ab o) in
+  if max_bl = a
   then "A"
-  else if max [ a; ab; o ] < b
+  else if max_bl = b
   then "B"
-  else if max [ a; b; o ] < ab
+  else if max_bl = ab
   then "AB"
   else "O"
 ;;
