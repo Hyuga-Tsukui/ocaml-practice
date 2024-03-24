931900246520d51e557662e4376ec084fc53a10e
Hyuga Tsukui <gakikiki407@gmail.com>
2024-03-24 10:38:36 +0900

    chapter10.6

--- chapter10/merge.ml
+++ chapter10/merge.ml
@@ -6,10 +6,7 @@ let rec merge lst1 lst2 =
   | [], first2 :: rest2 -> lst2
   | first1 :: rest1, [] -> lst1
   | first1 :: rest1, first2 :: rest2 ->
-    let merged_rest = merge rest1 rest2 in
-    if first1 < first2
-    then first1 :: first2 :: merged_rest
-    else first2 :: first1 :: merged_rest
+    if first1 < first2 then first1 :: merge rest1 lst2 else first2 :: merge rest2 lst1
 ;;
 
 (* tests *)
@@ -17,3 +14,4 @@ let test1 = merge [] [] = []
 let test2 = merge [] [ 1; 2 ] = [ 1; 2 ]
 let test3 = merge [ 1; 2 ] [] = [ 1; 2 ]
 let test4 = merge [ 2; 5 ] [ 1; 4 ] = [ 1; 2; 4; 5 ]
+let test5 = merge [ 1; 4 ] [ 1; 3 ] = [ 1; 1; 3; 4 ]
